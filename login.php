374632ad263ccab6c2b13f65a8779872293ddf9d
ghalya-kb <ghalyaqkq@gmail.com>
2022-05-14 00:29:11 +0300

    iletişim sayfası

--- login.php
+++ login.php
@@ -6,11 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="css/login.css">
+    <style>
+        .form p{
+            padding:60px;
+        }
+    </style>
     <title>Document</title>
 </head>
 <body>
     <div class="form">
         <p>
+
             <?php
                 if (($_POST['mail']=='b201210571@sakarya.edu.tr')&&($_POST['sifre']=='b201210571'))
                 {
@@ -19,14 +25,13 @@
                     echo'<a href="ana-sayfa.htm">Tıklayın</a>' ;          
                 }
                 else{
-                    echo"Yanlış Bilgi Girmişsiniz";
-                    header("Refresh:5; url=login.htm");
+                    echo"<strong>Yanlış Bilgi Girmişsiniz!!</strong> <br> <br> <strong>Giriş Sayfasına Yönlendireceksiniz.</strong>";
+                    header("Refresh:3; url=login.htm");
                 }
 
             ?> 
         </p>
     </div>
-    
 </body>
 </html>
 
